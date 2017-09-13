12612a96c4593d3a49863d65d2cb479e9243e5fc
Nathan Waid <natewaid@hotmail.com>
2017-09-13 08:18:25 -0400

    Add project files.

---
+++ Controllers/RmasLabelController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UAC.Reports.Controllers
+{
+    using Models;
+
+    public class RmasLabelController : Controller
+    {
+        [HttpGet]
+        [Route("rmaslabel/{manifestnumber:int?}/{planttype:int?}")]
+        public ActionResult Index(RmasLabelViewModel model = null)
+        {
+            return View(model ?? new RmasLabelViewModel());
+        }
+    }
+}
---
+++ Models/RmasLabelViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UAC.Reports.Models
+{
+    public class RmasLabelViewModel
+    {
+        public int ManifestNumber { get; set; }
+
+        public int PlantType { get; set; }
+    }
+}
